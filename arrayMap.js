8a761e51b9ff41489a4d0f52c3a70bf8d012eda7
Manan <manangupta4100@gmail.com>
2022-08-03 21:54:14 +0530

    new array

---
+++ arrayMap.js
@@ -0,0 +1,19 @@
+// 
+let n = [1,2,3,4,5,6,7,8,9,10]
+let m = 10
+
+let table = n.map((n)=> n*m)
+
+console.log(table)
+// 
+let array = ['Sam', 'Alice', 'Nick', 'Matt'];
+let newArray = array.map(name => {
+	return 'My name is ' + name; 
+});
+console.log(array)
+
+// 
+let arr = [1,4,9]
+let sqr = arr.map((arr) => Math.sqrt(arr))
+
+console.log(sqr)
---
+++ findLargestNo.js
@@ -0,0 +1,13 @@
+var Str_Array=['1','01','111','00001','11111','000001']
+
+var largest ='';
+// // Str_Array.forEach(function(item){
+//     if(item.length>largest.length)
+//         largest=item
+//     // });
+// console.log(largest)
+
+for(let item of Str_Array){
+    if(item.length>largest.length)largest
+}
+console.log(largest)
